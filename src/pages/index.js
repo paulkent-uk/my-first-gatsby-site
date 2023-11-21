4e03d6ecf79225ffd955d22dd0901d5c30e2c996
Paul Kent <pk@paulkent.biz>
2023-11-21 09:26:42 +0000

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Birthday card with guitar illustration"
+        src="../images/birthday_card.JPG"
+      />
     </Layout>
   )
 }
